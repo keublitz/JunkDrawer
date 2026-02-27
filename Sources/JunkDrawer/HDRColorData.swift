b4d3aee0c571ee9ecb68b891e8351f2ed4c9b0ce
keublitz <samuelwiener@gmail.com>
2026-02-27 15:37:30 -0500

    Add HDRColorData

---
+++ Sources/JunkDrawer/HDRColorData.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+import Dialogue
+
+/// A codable structure that can hold HDR color data.
+
+public struct HDRColorData: Codable {
+    private let r: Float
+    
+    private let g: Float
+    
+    private let b: Float
+    
+    private let alpha: Float
+    
+    public let sdr: ColorData
+    
+    public init(r: Float, g: Float, b: Float, alpha: Float = 1) {
+        self.r = r
+        self.g = g
+        self.b = b
+        self.alpha = alpha
+        
+        let SDRcolor = UIColor(
+            red: CGFloat(min(r, 1)),
+            green: CGFloat(min(g, 1)),
+            blue: CGFloat(min(b, 1)),
+            alpha: CGFloat(min(alpha, 1))
+        )
+        
+        self.sdr = SDRcolor.data
+    }
+}
+
+extension HDRColorData {
+    var color: Color? {
+        guard let colorSpace = CGColorSpace(name: CGColorSpace.extendedLinearSRGB),
+              let cgColor = CGColor(colorSpace: colorSpace, components: [CGFloat(self.r), CGFloat(self.g), CGFloat(self.b), CGFloat(self.alpha)]) else {
+            return nil
+        }
+        
+        return Color(cgColor: cgColor)
+    }
+}
+
+extension ColorData {
+    /// Returns data of the color converted to HDR by an amount of brightness.
+    ///
+    /// # Explanation
+    /// The luminance of the HDR color data is determined by a formula from the Rec. 709 standard, which is the color profile standard for HDTV and the basis for sRGB.
+    ///
+    /// ```swift
+    /// let luminance = r * 0.2126 + g * 0.7152 + b * 0.0722
+    /// ```
+    func hdr(brightness: Float = 1.5) -> HDRColorData {
+        // Rec. 709 luminance weight formula
+        let luminance = self.r * 0.2126 + self.g * 0.7152 + b * 0.0722
+        let boost = 1.0 + (brightness - 1.0) * luminance
+        
+        return HDRColorData(
+            r: self.r * boost,
+            g: self.g * boost,
+            b: self.b * boost,
+            alpha: self.alpha
+        )
+    }
+}
