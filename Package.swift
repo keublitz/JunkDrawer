226e03f40c4237aff0b29958dafaeb6f7e2ec493
keublitz <samuelwiener@gmail.com>
2026-02-27 02:09:56 -0500

    Add package dependency

--- Package.swift
+++ Package.swift
@@ -16,11 +16,19 @@ let package = Package(
             targets: ["JunkDrawer"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/yamoridon/ColorThiefSwift", from: "0.5.0"),
+        .package(url: "https://github.com/keublitz/Dialogue", from: "0.9.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JunkDrawer"
+            name: "JunkDrawer",
+            dependencies: [
+                .product(name: "ColorThiefSwift", package: "ColorThiefSwift"),
+                .product(name: "Dialogue", package: "Dialogue")
+            ]
         ),
     ]
 )
